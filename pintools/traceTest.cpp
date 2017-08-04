69a6f2f03955964deccbc07cf675631fe6ec0013
Van Mourik <gvanmou@s945308ca.srn.sandia.gov>
2017-08-04 15:50:14 -0700

    Adding mmap functionality to pintool traceTest.cpp

--- pintools/traceTest.cpp
+++ pintools/traceTest.cpp
@@ -8,14 +8,27 @@
 #include <pin.H>
 #include <string>
 #include <cstdlib>
+#include <stdlib.h>
 #include <iostream>
 
+#define SIZE sizeof(intptr_t)
 
 //Globals
 std::queue<intptr_t> traceStorage;
 
+
 //Additional Functions
-bool mmapInit(char *filePath, struct stat &memBuf, void *map, int &fd)
+size_t findTotalSize(size_t targetSize)
+{	
+	int i = 1;
+	while ( (PAGE_SIZE * i) < targetSize )
+	{
+		i++;
+	}
+	return (size_t)(PAGE_SIZE * i);
+}
+
+bool mmapInit(char *filePath, size_t fileSize, intptr_t *&map, int &fd)
 {        
         printf("File: %s\n", filePath);    
         fd = open(filePath, O_RDWR | O_CREAT | O_TRUNC, (mode_t)0600);
@@ -25,25 +38,29 @@ bool mmapInit(char *filePath, struct stat &memBuf, void *map, int &fd)
                 perror("File did not *open* properly");
                 return false;  
         }
-        if (stat(filePath, &memBuf) == -1)
-        {
-                perror("File status error (fstat)");
-                return false;
-        }
-        
-        map = mmap(NULL, memBuf.st_size, PROT_WRITE, MAP_SHARED, fd, 0);
-        if (map == MAP_FAILED)
+        //if (lseek(fd, fileSize, SEEK_SET) == -1)
+        //{
+        //        perror("Unable to appropriately size the file");
+        //        return false;
+        //}
+	//write(fd, "", 1); //needed to set the size
+       
+	printf("File size is...%lu bytes\n", fileSize); 
+        map = (intptr_t *)mmap(NULL, fileSize, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
+	if (map == MAP_FAILED)
         {
                 perror("mmap failed to open");
                 return false;
         }
-
-        return true;
+	printf("Map pointer init = %p\n", (void*)map);
+        
+	return true;
 }
 
-bool mmapClose(void *map, int &fd, struct stat &buf)
+bool mmapClose(intptr_t *&map, int &fd, size_t fileSize)
 {
-        if (munmap(map, buf.st_size) == -1)
+	printf("Map pointer close = %p\n", (void*)map);
+        if (munmap(map, fileSize) == -1)
         {
                 perror("munmap failed");
                 return false;
@@ -59,21 +76,39 @@ bool mmapClose(void *map, int &fd, struct stat &buf)
 
 void exportTrace()
 {
+	int length = 20;	
+
 	//Init mmap
 	int pinfd;
-	char *pinMap;
-	struct stat memBuf;
+	char *fileMem;
+	intptr_t *pinMap;
+	size_t traceLen = traceStorage.size();
+	size_t fileSize = traceLen * SIZE;
+	size_t totalSize = findTotalSize(fileSize);
 	char path[] = "/Users/gvanmou/Desktop/workflowProject/bin/mmapPin.out";
 	
-	mmapInit(path, memBuf, pinMap, pinfd);
+	mmapInit(path, totalSize, pinMap, pinfd);
 	
-	for (int i = 0; i < traceStorage.size(); i++)
+	printf("writing to file of length '%ld' bytes...\n", traceLen);
+	printf("Page size = '%d' bytes...\n", PAGE_SIZE);
+	for (size_t i = 0; i < traceLen; i++)
 	{
-		write(pinfd, &traceStorage.front(), sizeof(intptr_t));
+		//printf("    Trace Size = %ld\n", sizeof(traceStorage.front()));
+		printf("Before pinMap[%lu] = %lld\n", i, pinMap[i]);
+		pinMap[i] = traceStorage.front();
+		printf("After  pinMap[%lu] = %lld\n", i, pinMap[i]);
+
 		traceStorage.pop();
 	}
-	
-	mmapClose(pinMap, pinfd, memBuf);
+
+	printf("reading data from file...\n");
+	//for (int i = 0; i < traceLen; i++)
+	//{
+        //        printf("[%d]: %lld\n", i, pinMap[i]);
+	//} 
+		
+	printf("Map pointer middle = %p\n", (void*)pinMap);
+	mmapClose(pinMap, pinfd, totalSize);
 }
 
 
@@ -119,7 +154,7 @@ void Instruction( INS ins, void *v )
 void Fini( INT32 code, void *v )
 {
 	//Output collected trace to mmap
-	//exportTrace();
+	exportTrace();
 }
 
 
