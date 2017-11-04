1550d9af69f7036b193762690df1509917042e20
Garrett <garrettvanmourik@gmail.com>
2017-11-04 13:18:47 -0700

    "Passed the pintunnel as a variable through the pintool. Seg fault still occurs."

--- pintools/traceTool.cpp
+++ pintools/traceTool.cpp
@@ -8,19 +8,23 @@
 
 #include <PinTunnel.h>
 
+
 //Globals
 std::queue<trace_entry_t> traceData;
 UINT64 icount = 0;
-static PinTunnel *tunnel;
+//PinTunnel tunnel;
+// void *tunnel_void = nullptr;
+// int *test;
 const size_t buffer = 0;
+static std::ostream *Output = &std::cerr;
 
 //Declarations
-void recordMemRD( void *addr );
-void recordMemWR( void *addr );
+void recordMemRD( void *addr, void *tunnel_void );
+void recordMemWR( void *addr, void *tunnel_void );
 void Instruction( INS ins, void *v );
-void Fini( INT32 code, void *v );
-bool bufferCheckAndClear();
-bool bufferTransfer();
+void Fini( INT32 code, void *tunnel_void );
+bool bufferCheckAndClear(PinTunnel &tunnel);
+bool bufferTransfer(PinTunnel &tunnel);
 INT32 Usage();
 static bool signalHandler(unsigned int tid, int sig, LEVEL_VM::CONTEXT *ctx, bool hasHandler,
                           const LEVEL_BASE::EXCEPTION_INFO *exceptInfo, void *);
@@ -30,18 +34,23 @@ int main( int argc, char *argv[] )
 {
     if ( PIN_Init(argc, argv) ) { return Usage(); }
 
-    LEVEL_PINCLIENT::PIN_InterceptSignal(SIGSEGV,
-                                         signalHandler, nullptr);
+    // LEVEL_PINCLIENT::PIN_InterceptSignal(SIGSEGV,
+    //                                      signalHandler, nullptr);
 
-    PinTunnel tunnelInit; //THIS LINE as a global causes PIN to fail...
-    tunnel = &tunnelInit;
-    printf("Tunnel address [pinMain] = %p <-----\n", (void*)&tunnel);
-    printf("Tunnel buffer size (size != 0) = %zu\n", tunnel->getTunnelBufferLen(buffer) );
-    printf("Tunnel buffer readIndex (== 0) = %zu\n", tunnel->getTunnelReadIndex(buffer) );
+    PinTunnel tunnel_void; //THIS LINE as a global causes PIN to fail...
+    // tunnel_void = &tunnelInit;
+    // printf("Tunnel address [pinMain] = %p <-----\n", (void*)&tunnel);
+    // printf("Tunnel buffer size (size != 0) = %zu\n", tunnel->getTunnelBufferLen(buffer) );
+    // printf("Tunnel buffer readIndex (== 0) = %zu\n", tunnel->getTunnelReadIndex(buffer) );
 
-	INS_AddInstrumentFunction(Instruction, 0);
+    // int intInit = 0;
+    // test = &intInit;
+    // printf("Test int value = %d\n", *test );
 
-	PIN_AddFiniFunction(Fini, 0);
+	INS_AddInstrumentFunction(Instruction, &tunnel_void);
+
+	PIN_InitSymbols();
+    PIN_AddFiniFunction(Fini, &tunnel_void);
 
 	// Never returns
 	PIN_StartProgram();
@@ -52,11 +61,38 @@ int main( int argc, char *argv[] )
 
 
 //Pintool
-void recordMemRD( void *addr )
-{
-    if ( bufferCheckAndClear() )
+void Instruction( INS ins, void *tunnel_void )
+{   
+    UINT32 memOperands = INS_MemoryOperandCount(ins);
+    
+    for (UINT32 memOp = 0; memOp < memOperands; memOp++)
     {
-        printf("Buffer has been written to and cleared\n");
+        if (INS_MemoryOperandIsRead(ins, memOp))
+        {
+            INS_InsertPredicatedCall ( 
+                ins, IPOINT_BEFORE, (AFUNPTR)recordMemRD,
+                IARG_MEMORYOP_EA, memOp,
+                IARG_PTR, tunnel_void,
+                IARG_END);
+        }
+        if (INS_MemoryOperandIsWritten(ins, memOp))
+        {
+            INS_InsertPredicatedCall (
+                ins, IPOINT_BEFORE, (AFUNPTR)recordMemWR,
+                IARG_MEMORYOP_EA, memOp,
+                IARG_PTR, tunnel_void,
+                IARG_END);
+        }
+    }
+}
+
+void recordMemRD( void *addr, void *tunnel_void )
+{
+    PinTunnel *tunnel = reinterpret_cast<PinTunnel*>(tunnel_void);
+
+    if ( bufferCheckAndClear(*tunnel) )
+    {
+        // printf("Buffer has been written to and cleared\n");
         return;
     }
 	traceData.push( (trace_entry_t)addr );
@@ -64,40 +100,18 @@ void recordMemRD( void *addr )
 	//fprintf(trace, "%p\n", addr);
 }
 
-
-void recordMemWR( void *addr )
+void recordMemWR( void *addr, void *tunnel_void )
 {
-    if ( bufferCheckAndClear() )
+    PinTunnel *tunnel = reinterpret_cast<PinTunnel*>(tunnel_void);
+
+    if ( bufferCheckAndClear(*tunnel) )
     {
-        printf("Buffer has been written to and cleared\n");
+        // printf("Buffer has been written to and cleared\n");
         return;
     }
 	traceData.push( (trace_entry_t)addr );
 	icount++;
 	//fprintf(trace, "%p\n", addr);
-}
-
-void Instruction( INS ins, void *v )
-{	
-	UINT32 memOperands = INS_MemoryOperandCount(ins);
-	
-	for (UINT32 memOp = 0; memOp < memOperands; memOp++)
-	{
-		if (INS_MemoryOperandIsRead(ins, memOp))
-		{
-			INS_InsertPredicatedCall ( 
-				ins, IPOINT_BEFORE, (AFUNPTR)recordMemRD,
-                IARG_MEMORYOP_EA, memOp,
-                IARG_END);
-		}
-		if (INS_MemoryOperandIsWritten(ins, memOp))
-		{
-			INS_InsertPredicatedCall (
-				ins, IPOINT_BEFORE, (AFUNPTR)recordMemWR,
-                IARG_MEMORYOP_EA, memOp,
-                IARG_END);
-		}
-	}
 }
 
 INT32 Usage()
@@ -107,28 +121,30 @@ INT32 Usage()
     return -1;
 }
 
-void Fini( INT32 code, void *v )
+void Fini( INT32 code, void *tunnel_void )
 {
+    PinTunnel *tunnel = reinterpret_cast<PinTunnel*>(tunnel_void);
+
 	//One last transfer to clear up any straglers
 	printf("Before final buffer transfer...\n");
-    bufferTransfer();
+    bufferTransfer(*tunnel);
 
 	printf("\nPIN is finished...goodbye...\n");
 }
 
-static bool signalHandler(unsigned int tid, int sig, LEVEL_VM::CONTEXT *ctx, bool hasHandler,
-                          const LEVEL_BASE::EXCEPTION_INFO *exceptInfo, void *)
-{
-    printf("Process[%d] Thread[%d] recieved signal %d\n...waiting...\n", getpid(), tid, sig);
-    sleep(10);
+// static bool signalHandler(unsigned int tid, int sig, LEVEL_VM::CONTEXT *ctx, bool hasHandler,
+//                           const LEVEL_BASE::EXCEPTION_INFO *exceptInfo, void *)
+// {
+//     printf("Process[%d] Thread[%d] recieved signal %d\n...waiting...\n", getpid(), tid, sig);
+//     sleep(10);
 
-    exit(0);
-    return true;
-}
+//     exit(0);
+//     return true;
+// }
 
 
 //Additional Functions
-bool bufferCheckAndClear()
+bool bufferCheckAndClear(PinTunnel &tunnel)
 {
     //printf("IN bufferCheckAndClear()... [%llu]\n", icount);
     if ( icount < (WORKSPACE_LEN) )
@@ -137,23 +153,22 @@ bool bufferCheckAndClear()
     }
 
     //WRITE TO BUFFER
-    return bufferTransfer();
+    return bufferTransfer(tunnel);
 }
 
-bool bufferTransfer()
+bool bufferTransfer(PinTunnel &tunnel)
 {
-    //write to buffer
+    // write to buffer
     printf("IN bufferTransfer()...\n");
     printf("Tunnel address [bufferTransfer] = %p <-----\n", (void*)&tunnel);
     printf("Elements to add to buffer = %zu\n", traceData.size() );
     
-    printf("Tunnel buffer size (size != 0) and again... = %zu\n", tunnel->getTunnelBufferLen(buffer) );
-    int transferCount = tunnel->writeTraceSegment(buffer, traceData);
+    // printf("Test int value [bufferTransfer] = %d\n", *test );
+    printf("Tunnel buffer size (size != 0) and again... = %zu\n", tunnel.getTunnelBufferLen(buffer) );
+    int transferCount = tunnel.writeTraceSegment(buffer, traceData);
 
     size_t transferSizeActual = transferCount * sizeof(trace_entry_t);
     printf("Actual number of bytes transferred = %lu\n", transferSizeActual);
     icount = 0;
     return true;
 }
-
-
